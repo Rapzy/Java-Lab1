f6dc4750dc84bdd8ad63f3c503f44a7a1d54a4ee
Rapzy <andrushko.egor@gmail.com>
2019-09-17 09:55:37 +0300

    Add Hotel class

---
+++ src/hotel/Hotel.java
@@ -0,0 +1,78 @@
+package hotel;
+import java.util.ArrayList;
+
+public class Hotel {
+    private String name;
+    private String address;
+    private Integer stars;
+    private ArrayList<Room> rooms;
+
+    public Hotel(String name, String address, Integer stars, Integer roomsNum){
+        this.name = name;
+        this.address = address;
+        this.stars = stars;
+        this.rooms = new ArrayList<Room>();
+        for (int i = 0; i < roomsNum; i++){
+            rooms.add(new Room(i+1));
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getStars() {
+        return stars;
+    }
+
+    public void setStars(Integer stars) {
+        this.stars = stars;
+    }
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(ArrayList<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+
+    public class Room {
+        private Integer number;
+        private Boolean isFree;
+
+        public Room(Integer number){
+            this.number = number;
+            this.isFree = true;
+        }
+
+        public Integer getNumber() {
+            return number;
+        }
+
+        public void setNumber(Integer number) {
+            this.number = number;
+        }
+
+        public Boolean getFree() {
+            return isFree;
+        }
+
+        public void setFree(Boolean free) {
+            isFree = free;
+        }
+    }
+}
